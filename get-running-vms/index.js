a886fbccff74a6b6851f0096ad5f8a0a60556d8f
dzeyelid <dzeyelid@gmail.com>
2018-04-15 09:25:24 +0900

    Added get-running-vms function.

---
+++ get-running-vms/index.js
@@ -0,0 +1,33 @@
+const rp = require('request-promise');
+const msRestAzure  = require("ms-rest-azure");
+const computeManagementClient = require('azure-arm-compute');
+
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    status = 400;
+    body = "Something is wrong. :(";
+
+    subscriptionId = process.env['subscriptionId'];
+
+    msRest.Azure.loginWithAppServiceMSI()
+        .then(function(credencials) {
+            const computeClient = new computeManagementClient(credencials, subscriptionId);
+
+            computeClient.VirtualMachines.listAll()
+                .then(function(res)) {
+                    status = 200;
+                    body = JSON.stringify(res);
+                }
+        })
+        .catch(function(err) {
+            context.log('err: ' + err);
+            status = 500;
+            body = JSON.stringify({'error': err});
+        });
+
+    content.res = {
+        status: status,
+        body: body
+    }
+    context.done();
+};
