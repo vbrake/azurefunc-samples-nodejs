8adffb033bf14cd075e8e7c0aca079452cfb8c01
dzeyelid <dzeyelid@gmail.com>
2018-04-15 20:37:48 +0900

    test

--- get-running-vms/index.js
+++ get-running-vms/index.js
@@ -20,11 +20,10 @@ function done(context, status, body) {
 }
 
 function getVirtualMachinesRunning(context, credentials, subscriptionId) {
-    return 'aaa';
-    // var computeClient = new computeManagementClient(credentials, subscriptionId);
+    var computeClient = new computeManagementClient(credentials, subscriptionId);
 
-    // try {
-    //     let vms = await computeClient.virtualMachines.listAll();
+    try {
+        let vms = await computeClient.virtualMachines.listAll();
         // var instanceViews = mvs.map(vm => {
         //     var filterRG = new RegExp('\/subscriptions\/.+?\/resourceGroups\/(.+?)\/.*?$');
         //     filtered = filterRG.exec(vm.id);
@@ -39,10 +38,11 @@ function getVirtualMachinesRunning(context, credentials, subscriptionId) {
         //     // return instanceView;
         // });
         // done(context, 200, instanceViews);
-    // }
-    // catch (err) {
-    //     doneWithError(context, err);
-    // }
+        return vms;
+    }
+    catch (err) {
+        doneWithError(context, err);
+    }
 }
 
 module.exports = function (context, req) {
