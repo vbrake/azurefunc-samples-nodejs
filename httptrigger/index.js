af3375c3b2f04fb27b113e9c96edb10d084b4669
Walter Freitas Jr <WJ00767022@techmahindra.com>
2021-09-28 15:13:53 -0300

    Mudanca no texto do index.js da funcao httptrigger

--- httptrigger/index.js
+++ httptrigger/index.js
@@ -4,13 +4,13 @@ module.exports = function(context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+		body: "Teste da funcao httptrigger em pipeline. Parametro name =  " + (req.query.name || req.body.name)
         };
     }
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Favor passar um nome na query string ou no request body"
         };
     }
     context.done();
